fdab7d35323c7df12c3ff395346dce9fa9751779
arthursvpb <arthurandroid15@gmail.com>
2020-12-08 14:28:38 -0300

    delete person ok

--- web/src/pages/CreatePerson.js
+++ web/src/pages/CreatePerson.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import api from "../services/api";
 
 function CreatePerson() {
-  return <h1>Create Person</h1>;
+  const history = useHistory();
+
+  const [cpf, setCpf] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [nationality, setNationality] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = {
+      cpf,
+      name,
+      email,
+      birthday,
+      nationality,
+    };
+    try {
+      await api.post("/person", data).then();
+      alert("Success");
+    } catch (err) {
+      alert("Error");
+    }
+
+    history.push("/person");
+  }
+
+  return (
+    <div id="page-create-person">
+      <h1>Create Person</h1>
+      <main>
+        <form onSubmit={handleSubmit} className="create-person-form">
+          <fieldset>
+            <legend>Data</legend>
+
+            <label htmlFor="name">Name</label>
+            <input
+              className="input-block"
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+
+            <label htmlFor="cpf">CPF</label>
+            <input
+              className="input-block"
+              id="cpf"
+              value={cpf}
+              onChange={(event) => setCpf(event.target.value)}
+            />
+
+            <label htmlFor="birthday">birthday</label>
+            <input
+              className="input-block"
+              id="birthday"
+              value={birthday}
+              onChange={(event) => setBirthday(event.target.value)}
+            />
+
+            <label htmlFor="email">E-mail</label>
+            <input
+              className="input-block"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+
+            <label htmlFor="nationality">Nationality</label>
+            <input
+              className="input-block"
+              id="nationality"
+              value={nationality}
+              onChange={(event) => setNationality(event.target.value)}
+            />
+          </fieldset>
+          <button className="confirm-button" type="submit">
+            Confirm
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default CreatePerson;
--- web/src/pages/Person.js
+++ web/src/pages/Person.js
@@ -15,9 +15,18 @@ function Person() {
     });
   }, []);
 
+  async function handleRemovePerson(id) {
+    await api.delete(`/person/${id}`);
+
+    setPersons(persons.filter((person) => person.id !== id));
+  }
+
   return (
     <div>
       <h1>Person list</h1>
+      <a href="/person/new" id="enter-create-person">
+        Create new person
+      </a>
       {persons.map((person) => {
         return (
           <div className="person-list">
@@ -27,12 +36,17 @@ function Person() {
                 <img src={user} alt="User" width={60}></img>
               </div>
               <div>
+                <p>ID: {person.id}</p>
                 <p>CPF: {person.cpf}</p>
                 <p>E-mail: {person.email}</p>
                 <p>Birthday: {person.birthday}</p>
                 <p>Nationality: {person.nationality}</p>
                 <p>Created at: {person.createdAt}</p>
                 <p>Updated at: {person.updatedAt}</p>
+                <a href={`/person/${person.id}/edit`}>Update</a>
+                <button onClick={() => handleRemovePerson(person.id)}>
+                  Delete
+                </button>
               </div>
             </div>
           </div>
