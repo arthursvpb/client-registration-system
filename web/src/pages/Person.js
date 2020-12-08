a29982cd79662de990f9c0a7bd41b9b40e9a973f
arthursvpb <arthurandroid15@gmail.com>
2020-12-08 11:18:33 -0300

    listing persons and adding some style

--- web/src/pages/Person.js
+++ web/src/pages/Person.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 
 import api from "../services/api";
 
+import user from "../images/user.png";
+
+import "../styles/pages/person.css";
+
 function Person() {
   const [persons, setPersons] = useState([]);
 
@@ -11,9 +15,31 @@ function Person() {
     });
   }, []);
 
-  console.log(persons);
-
-  return <h1>Person</h1>;
+  return (
+    <div>
+      <h1>Person list</h1>
+      {persons.map((person) => {
+        return (
+          <div className="person-list">
+            <div className="person">
+              <div className="user">
+                <h2>{person.name}</h2>
+                <img src={user} alt="User" width={60}></img>
+              </div>
+              <div>
+                <p>CPF: {person.cpf}</p>
+                <p>E-mail: {person.email}</p>
+                <p>Birthday: {person.birthday}</p>
+                <p>Nationality: {person.nationality}</p>
+                <p>Created at: {person.createdAt}</p>
+                <p>Updated at: {person.updatedAt}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Person;
