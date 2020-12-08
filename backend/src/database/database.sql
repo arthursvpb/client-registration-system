ccf471827f9c07951ef49a3adbc958f7f7e60e63
Arthur Vasconcellos <arthurandroid15@gmail.com>
2020-12-07 22:54:34 -0300

    Update database.sql

--- backend/src/database/database.sql
+++ backend/src/database/database.sql
@@ -2,13 +2,13 @@ CREATE DATABASE clientregistrationsystem;
 USE clientregistrationsystem;
 
 CREATE TABLE Person(
-	id int auto_increment not null,
+    id int auto_increment not null,
     cpf varchar(20) not null,
     name varchar(100) not null,
     email varchar(100),
     birthday date not null,
     nationality varchar(20),
-	createdAt date,
+    createdAt date,
     updatedAt date,
-	primary key(id, cpf)
+    primary key(id, cpf)
 );
